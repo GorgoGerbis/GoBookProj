d72e23f7c948130fec124bf3e1b936699404d9f4
GorgoGerbis <jacksonwalker5121@gmail.com>
2024-12-06 17:14:05 -0500

    Finished section 1.2

---
+++ cmd/token/token.go
@@ -0,0 +1,44 @@
+// Chapter-1:
+// - Going to write our own lexer.
+// - Lexer will take source code as input and output the tokens that represent the source code.
+// - Lexer reads in source code as a string, analyzes it character by character, and output the next
+//   token it recognizes. 
+// - ONLY need ONE method NextToken() which just outputs the next token
+
+
+// token/token.go
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+// We have a LIMITED number of token types so we can define them as CONSTANTS
+const (
+	ILLEGAL = "ILLEGAL" // Illegal signifies a character or token we dont know about
+	EOF = "EOF" // End of file
+
+	// Identifiers + Literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT" // 13456
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	// Delimeters 
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
