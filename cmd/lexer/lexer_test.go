36058324c8ef1316d69694915372555b67837e9b
GorgoGerbis <jacksonwalker5121@gmail.com>
2024-12-06 17:20:06 -0500

    Started section 1.3.

---
+++ cmd/lexer/lexer_test.go
@@ -0,0 +1,14 @@
+// Chapter-1: Section 1.3
+//
+// lexer/lexer_test.go
+
+// package lexer
+
+// import (
+// 	"testing"
+
+// 	"monkey/token"
+// )
+
+// func TestNextToken(t *testing.T){
+// 	input := `=_(){},;`
