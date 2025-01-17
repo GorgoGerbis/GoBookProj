7dadd64e8498b8d698e3c9666cea0fca9c578457
GorgoGerbis <jacksonwalker5121@gmail.com>
2025-01-17 03:18:03 -0500

    FINISHED CHAPTER 1 FINALLYgit add .! Learned a TON about go and all taht! I know have a working lexer, lexer test-cases and a functioning REPL!

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -1,11 +1,22 @@
+// main.go
+
 package main // Declare a main package (a package is a way to group functions, and it's made up of all the files in the same directory).
 
 import (
-	"fmt" // Import the popular fmt package, which contains functions for formatting text, including printing to the console. This package is one of the standard library packages you got when you installed Go.
+	// Standard library packages:
+	"fmt" // Import fmt package: Contains functions for formatting text, including printing to the console.
+	"os"
+	"os/user"
+	"monkey/repl"
 )
 
-// A main function executes by default when you run the main package.
+// A main function executes by default when you run 'go run .' the main package.
 func main() {
-	fmt.Println("Hello, World!")
-	// run: 'go run .'
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("HEllo %s! This is the Monkey Programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
